5bf4b165cf140485ca8ac46e3ee913f3c639058b
jefti <jeftimeira@gmail.com>
2023-10-04 12:30:14 -0300

    exercicio 07

--- aula-testes-07/tests/unit/infractions.test.ts
+++ aula-testes-07/tests/unit/infractions.test.ts
@@ -1,11 +1,40 @@
+import * as usersRepositories from "../../src/users-repository";
+import * as infractionsRepository from "../../src/infractions-repository";
+import { getInfractionsFrom } from "infractions-service";
+
 describe("Infractions Service Tests", () => {
   it("should get infractions from user", async () => {
-    // TODO
-    expect(true).toBe(true);
+    jest.spyOn(usersRepositories, "getUserByDocument").mockImplementationOnce((licenseId: string):any=>{
+      return {
+        id: 0,
+        firstName: 'john',
+        lastName: 'Doe',
+        licenseId 
+      }
+    });
+    jest.spyOn(infractionsRepository, "getInfractionsFrom").mockImplementationOnce((userId: number):any=>{
+      return [];
+    });
+    const licenseId = 'teste';
+    const resp = await getInfractionsFrom(licenseId);
+    expect(resp).toBe(expect.objectContaining({
+      id: 0,
+      firstName: 'john',
+      lastName: 'Doe',
+      licenseId,
+      infractions: []
+      })
+    );
   });
 
   it("should throw an error when driver license does not exists", () => {
-    // TODO
-    expect(true).toBe(true);
+    jest.spyOn(usersRepositories, "getUserByDocument").mockImplementationOnce((licenseId: string):any=>{
+      return {}
+    });
+    const licenseId = 'teste';
+    const resp =  getInfractionsFrom(licenseId);
+    expect(resp).toBe(expect.objectContaining(
+      {type: "NOT_FOUND", message: "Driver not found."}
+    ));
   })
 });
